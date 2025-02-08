3eb6fb15b539436c848f1a42db15464c9f1427ef
miazi2003 <yeasinmiazi1997@gmail.com>
2025-02-08 20:31:43 +0600

    update

--- arrey.js
+++ arrey.js
@@ -7,10 +7,3 @@ let text = " a a a a a"
 
 console.log(Array.isArray(text))
 
-echo "# js-practice" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/miazi2003/js-practice.git
-git push -u origin main
