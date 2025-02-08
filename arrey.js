fdcac446e95e363cd81eb8266c093f03959b9b65
miazi2003 <yeasinmiazi1997@gmail.com>
2025-02-08 20:36:56 +0600

    update

--- arrey.js
+++ arrey.js
@@ -1,9 +1,11 @@
 let friends = ['a','b','c','d','e'];
-let nums = [];
+let nums = [1,2,3,4];
 let text = " a a a a a"
 // console.log(friends.includes('f'))
 
 // console.log(friends.indexOf('g'))
 
-console.log(Array.isArray(text))
+// console.log(Array.isArray(text))
+
+// console.log(friends.concat(nums));
 
