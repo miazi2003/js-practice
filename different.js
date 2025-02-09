302002528b91697b10711d85a74a4495d384b7a1
miazi2003 <yeasinmiazi1997@gmail.com>
2025-02-09 21:30:53 +0600

    updated again

--- different.js
+++ different.js
@@ -1,5 +1,8 @@
-for (let i = 1; i < 20; i++) {
-    if(i%2===0){
-        console.log(i)
-    }
+let sum = 0;
+
+
+for (let i = 2; i < 2000000; i++) {
+        sum = sum + i
+        
 }
+console.log(sum)
