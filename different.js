0aeffbdc63ec8482b94cef55aca8dd88766af292
miazi2003 <yeasinmiazi1997@gmail.com>
2025-02-10 23:22:35 +0600

    reverse

--- different.js
+++ different.js
@@ -9,8 +9,19 @@
 // console.log(Real2.join('-----'))
 // console.log(Real2)
 
-let first = "abid";
-let last = "nabid";
+// let first = "abid";
+// let last = "nabid";
 
-let fullName = first.concat(" ").concat(last);
-console.log(fullName)
+// let fullName = first.concat(" ").concat(last);
+// console.log(fullName)
+
+
+let sen = "i am a programmer";
+
+let reverse = ''
+for (const letter of sen ){
+    // console.log(letter)
+    reverse = letter + reverse
+}
+
+console.log(reverse)
