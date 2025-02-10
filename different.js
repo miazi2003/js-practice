98faa4c3f59c0f8b830485132f8607f8b04090a8
miazi2003 <yeasinmiazi1997@gmail.com>
2025-02-10 23:06:35 +0600

    concat

--- different.js
+++ different.js
@@ -3,8 +3,14 @@
 // console.log(frnd)
 // console.log(frnd[1])
 
-let real = "rahim ,  fahim , karim , sahil";
-let Real2 = real.split(",")
-console.log(Real2.join())
-console.log(Real2.join('-----'))
-console.log(Real2)
+// let real = "rahim ,  fahim , karim , sahil";
+// let Real2 = real.split(",")
+// console.log(Real2.join())
+// console.log(Real2.join('-----'))
+// console.log(Real2)
+
+let first = "abid";
+let last = "nabid";
+
+let fullName = first.concat(" ").concat(last);
+console.log(fullName)
