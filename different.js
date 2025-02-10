1a68b79df84c73e95be5bcc0f6b85442a7d3a0a5
miazi2003 <yeasinmiazi1997@gmail.com>
2025-02-10 23:02:28 +0600

    join

--- different.js
+++ different.js
@@ -1,4 +1,10 @@
-let frndStr = "rahim ,  fahim , karim , sahil";
-let frnd = frndStr.split(",")
-console.log(frnd)
-console.log(frnd[1])
+// let frndStr = "rahim ,  fahim , karim , sahil";
+// let frnd = frndStr.split(",")
+// console.log(frnd)
+// console.log(frnd[1])
+
+let real = "rahim ,  fahim , karim , sahil";
+let Real2 = real.split(",")
+console.log(Real2.join())
+console.log(Real2.join('-----'))
+console.log(Real2)
