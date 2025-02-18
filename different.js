333a961c4be7dbd2c50f83e4793180942e149074
miazi2003 <yeasinmiazi1997@gmail.com>
2025-02-18 21:59:54 +0600

    update

--- different.js
+++ different.js
@@ -15,7 +15,6 @@
 // let fullName = first.concat(" ").concat(last);
 // console.log(fullName)
 
-
 // let sen = "i am a programmer";
 
 // let reverse = ''
@@ -26,12 +25,9 @@
 
 // // console.log(reverse)
 
-
- 
 // let reversed = sen.split(' ').reverse().join(' ')
 
 // console.log(reversed)
-
 
 // let car = {
 //     brand : "BMW",
@@ -44,19 +40,16 @@
 
 // car.accelerate()
 
-
 // function vatKhao(){
 //     console.log("ppp")
 // }
 // vatKhao();
-
 
 // function square (number){
 // const borgo = number * number;
 // console.log(borgo)
 // }
 // square(3);
-
 
 // function doubleIt (number1 , number2){
 //     const double = number1 - number2;
@@ -68,15 +61,12 @@
 // let fAge = 59;
 // doubleIt( fAge , myAge);
 
-
 // function tenTimes(number){
 //     const result = number * 10;
 //     return result
 // }
 //  let bigNum = tenTimes(2);
 //  console.log(bigNum)
-
-
 
 // function isLeapYear(year){
 //     if(year % 4 === 0){
@@ -89,7 +79,6 @@
 
 // const year = isLeapYear(2024)
 // console.log(year)
-
 
 // function isLeapYear2(year){
 //      if (year % 100 !== 0 && year % 4 === 0){
@@ -106,3 +95,23 @@
 // const year = isLeapYear2(2054)
 
 // console.log(year)
+
+function oddAvg(numbers) {
+  let odds = [];
+  for (const number of numbers) {
+    if (number % 2 === 1) {
+      odds.push(number);
+    }
+  }
+  let sum = 0;
+  for (const number of odds) {
+    sum = sum + number;
+  }
+  const count = odds.length;
+  console.log(sum, count);
+  const avg = sum / count;
+  return avg;
+}
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 13];
+const avg = oddAvg(numbers);
+console.log(avg);
